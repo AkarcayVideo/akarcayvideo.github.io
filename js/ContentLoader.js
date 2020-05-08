3e0cb6dbaeb942c7f9c5cb2944cb9a620754438d
AkarcayVideo <akarcayvideo@users.noreply.github.com>
2020-05-09 02:36:09 +0300

    Added Content Viewer System

--- js/ContentLoader.js
+++ js/ContentLoader.js
@@ -1,7 +1,7 @@
-function LoadContent(index, parentId, folderName, fileName, fileExtension) {
+function LoadContent(index, folderName, fileName, fileExtension) {
     const baseURL = "https://raw.githubusercontent.com/AkarcayVideo/akarcayvideo.github.io/master/icerik";
     const link = `${baseURL}/${folderName}/${fileName}${index}.${fileExtension}`;
-    const parent = document.getElementById(parentId);
+    const parent = document.getElementById("content-holder");
 
     const LoadIfExists = (url, callback) => {
         const request = new XMLHttpRequest();
@@ -16,19 +16,66 @@ function LoadContent(index, parentId, folderName, fileName, fileExtension) {
     LoadIfExists(link, exists => {
         if (exists) {
 
+            let element = null;
+
             if (fileExtension == "jpeg" || fileExtension == "jpg") {
-                parent.innerHTML += `<img src="${link}">`;
+
+                const image = document.createElement("img");
+                image.src = link;
+                element = image;
+
             }
             else if (fileExtension == "mp4") {
-                parent.innerHTML += `
-                <video controls>
-                    <source src="${link}" type="video/mp4">
-                </video>
-                `;
+
+                const video = document.createElement("video");
+                video.controls = true;
+                const source = document.createElement("source");
+                source.src = link;
+                source.type = "video/mp4";
+
+                video.appendChild(source);
+                element = video;
+
             }
             else { console.log("File extension not implemented.") }
 
-            LoadContent(index + 1, parentId, folderName, fileName, fileExtension);
+            if (element) {
+                parent.appendChild(element);
+                element.addEventListener("click", () => ShowContentViewer(link, fileExtension));
+            }
+
+            LoadContent(index + 1, folderName, fileName, fileExtension);
         }
     })
 }
+
+function ShowContentViewer(link, extension) {
+    const viewer = document.getElementById("content-viewer");
+    viewer.style.display = "flex";
+
+    const content = viewer.querySelector(".content");
+
+    if (extension == "jpeg" || extension == "jpg") {
+        content.innerHTML = `<img src=${link}>`;
+    }
+    else if (extension == "mp4") {
+        content.innerHTML = `<video controls><source src=${link} type="video/mp4"></source></video>`;
+    }
+}
+
+// CLOSE CONTENT VIEWER
+(function () {
+
+    const viewer = document.getElementById("content-viewer");
+
+    viewer.addEventListener("click", (e) => {
+
+        const y = (e.clientY / window.innerHeight);
+
+        if (y <= .25 || y >= .75) {
+            viewer.style.display = "none";
+        }
+
+    });
+
+})()
