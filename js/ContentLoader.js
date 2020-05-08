d7623c8615d806a469186e9ce43bed6fe957d538
AkarcayVideo <akarcayvideo@users.noreply.github.com>
2020-05-08 21:42:11 +0300

    Video System

---
+++ js/ContentLoader.js
@@ -0,0 +1,33 @@
+function LoadContent(index, parentId, folderName, fileName, fileExtension) {
+    const link = `https://akarcayvideo.github.io/icerik/${folderName}/${fileName}${index}.${fileExtension}`;
+    const parent = document.getElementById(parentId);
+
+    const LoadIfExists = (url, callback) => {
+        const request = new XMLHttpRequest();
+        request.onreadystatechange = () => {
+            if (request.readyState == 2)
+                callback(request.status == 200);
+        }
+        request.open("GET", url, true);
+        request.send(null);
+    }
+
+    LoadIfExists(link, exists => {
+        if (exists) {
+
+            if (fileExtension == "jpeg" || fileExtension == "jpg") {
+                parent.innerHTML += `<img src="${link}">`;
+            }
+            else if (fileExtension == "mp4") {
+                parent.innerHTML += `
+                <video controls>
+                    <source src="${link}" type="video/mp4">
+                </video>
+                `;
+            }
+            else { console.log("File extension not implemented.") }
+
+            LoadContent(index + 1, parentId, folderName, fileName, fileExtension);
+        }
+    })
+}
