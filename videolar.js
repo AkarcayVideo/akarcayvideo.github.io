9ff99c51d268b7c92552098a678a3e0587ad194b
AkarcayVideo <64988010+AkarcayVideo@users.noreply.github.com>
2020-05-09 16:54:53 +0300

    Update videolar.js

--- videolar.js
+++ videolar.js
@@ -2,6 +2,7 @@ const VIDEOS = {
     "anasayfa": [
         "https://www.youtube.com/embed/M0xxa-46MTY",
         "https://www.youtube.com/embed/M0xxa-46MTY",
+        "https://www.youtube.com/embed/ihR8_unPjBE",
     ],
     "dugun": [
         "https://www.youtube.com/embed/M0xxa-46MTY",
