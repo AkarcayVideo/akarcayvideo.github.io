661d2557820728d72c500c76a66c944d8892f16c
Berkin AKKAYA <berkin_akkaya@hotmail.com>
2020-05-10 22:57:01 +0300

    JavaScript files merged

--- js/ContentLoader.js
+++ js/ContentLoader.js
@@ -1,4 +1,5 @@
 let LAST_INDEX = 0;
+let LAST_SCROLL_POSITION = 0;
 
 function GenerateImageLink(index) {
     const baseURL = "https://raw.githubusercontent.com/AkarcayVideo/akarcayvideo.github.io/master/";
@@ -49,3 +50,60 @@ function LoadVideos(page) {
 
     LAST_INDEX = VIDEOS[page].length;
 }
+
+function ShowImageViewer(index) {
+    const viewer = document.getElementById("content-viewer");
+    const link = GenerateImageLink(index);
+    viewer.style.display = "flex";
+    LAST_SCROLL_POSITION = window.scrollY;
+    ScrollLock(true);
+
+    const content = viewer.querySelector(".content");
+    content.innerHTML = `<img src=${link}>`;
+
+    const left = viewer.querySelector(".left");
+    const right = viewer.querySelector(".right");
+    left.style.visibility = index == 1 ? "hidden" : "visible";
+    right.style.visibility = index == LAST_INDEX ? "hidden" : "visible";
+
+    left.onclick = () => ShowImageViewer(index - 1);
+    right.onclick = () => ShowImageViewer(index + 1);
+}
+
+function ScrollLock(lock) {
+
+    const body = document.querySelector("body");
+
+    if (lock) {
+
+        window.scrollTo(0, 0);
+        body.style.height = "100vh";
+        body.style.overflowY = "hidden";
+
+    } else {
+
+        body.style.height = "unset";
+        body.style.overflowY = "visible";
+        window.scrollTo(0, LAST_SCROLL_POSITION);
+
+    }
+
+}
+
+// CLOSE CONTENT VIEWER
+(function () {
+
+    const viewer = document.getElementById("content-viewer");
+
+    viewer.addEventListener("click", (e) => {
+
+        const y = (e.clientY / window.innerHeight);
+
+        if (y <= .3 || y >= .7) {
+            viewer.style.display = "none";
+            ScrollLock(false);
+        }
+
+    });
+
+})()
